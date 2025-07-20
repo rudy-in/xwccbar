2c74231c040759b925f348a46c4ceaae8450a249
rudy-in <msharmion@gmail.com>
2025-07-20 18:52:39 +0530

    CORE: Wayland Support

---
+++ Makefile
@@ -0,0 +1,16 @@
+PKG_CONFIG = pkg-config
+PKG_FLAGS = gtk+-3.0 gtk-layer-shell-0
+CXX = g++
+CXXFLAGS = -Wall -g $(shell $(PKG_CONFIG) --cflags $(PKG_FLAGS))
+LDFLAGS = $(shell $(PKG_CONFIG) --libs $(PKG_FLAGS)) -lX11
+
+SRC = src/main.cc
+OUT = mygtkapp
+
+all: $(OUT)
+
+$(OUT): $(SRC)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
+
+clean:
+	rm -f $(OUT)
--- src/main.cc
+++ src/main.cc
@@ -1,43 +1,81 @@
+#define GDK_WINDOWING_WAYLAND
+#define GDK_WINDOWING_X11
 #include <gtk/gtk.h>
-#include <gdk/gdkx.h> // Needed for X11 hints
+
+#ifdef GDK_WINDOWING_X11
+#include <gdk/gdkx.h>
+#include <X11/Xatom.h>
+#endif
+
+#ifdef GDK_WINDOWING_WAYLAND
+#include <gdk/gdkwayland.h>
+#include <gtk-layer-shell/gtk-layer-shell.h>
+#endif
+
+
+void set_x11_dock_hint(GtkWidget *window) {
+#ifdef GDK_WINDOWING_X11
+    GdkWindow *gdk_window = gtk_widget_get_window(window);
+    if (!GDK_IS_X11_WINDOW(gdk_window)) return;
+
+    Display *xdisplay = GDK_WINDOW_XDISPLAY(gdk_window);
+    Window xwindow = GDK_WINDOW_XID(gdk_window);
+
+    Atom window_type_atom = XInternAtom(xdisplay, "_NET_WM_WINDOW_TYPE", False);
+    Atom dock_atom = XInternAtom(xdisplay, "_NET_WM_WINDOW_TYPE_DOCK", False);
+    XChangeProperty(xdisplay, xwindow, window_type_atom, XA_ATOM, 32,
+                    PropModeReplace, (unsigned char *) &dock_atom, 1);
+#endif
+}
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "GTK Panel");
-
-    // Remove window decorations (borderless)
     gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
-    // Always on top
-    gtk_window_set_keep_above(GTK_WINDOW(window), TRUE);
+    GdkDisplay *display = gdk_display_get_default();
+    gboolean is_wayland = GDK_IS_WAYLAND_DISPLAY(display);
+    gboolean is_x11 = GDK_IS_X11_DISPLAY(display);
 
-    // Set size and position
-    gtk_window_set_default_size(GTK_WINDOW(window), 1920, 30); // width, height
-    gtk_window_move(GTK_WINDOW(window), 0, 0); // position at top
 
-    // Set window type hint to "dock"
-    gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DOCK);
+    if (is_wayland) {
+#ifdef GDK_WINDOWING_WAYLAND
+        gtk_layer_init_for_window(GTK_WINDOW(window));
+        gtk_layer_set_layer(GTK_WINDOW(window), GTK_LAYER_SHELL_LAYER_TOP);
+        gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_TOP, TRUE);
+        gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
+        gtk_layer_set_anchor(GTK_WINDOW(window), GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
+        gtk_layer_auto_exclusive_zone_enable(GTK_WINDOW(window));
+        gtk_window_set_default_size(GTK_WINDOW(window), 1, 30); // width doesn't matter here
+#endif
+    } else if (is_x11) {
+        gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DOCK);
+        gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window), TRUE);
+        gtk_window_set_skip_pager_hint(GTK_WINDOW(window), TRUE);
+        gtk_window_set_keep_above(GTK_WINDOW(window), TRUE);
+        gtk_window_set_default_size(GTK_WINDOW(window), 1920, 30);
+        gtk_window_move(GTK_WINDOW(window), 0, 0);
 
-    // Optional: background color
-    GdkRGBA color;
-    gdk_rgba_parse(&color, "#222222");
+        g_signal_connect(window, "realize", G_CALLBACK(set_x11_dock_hint), NULL);
+    }
+
+    // CSS
     GtkCssProvider *provider = gtk_css_provider_new();
     gtk_css_provider_load_from_data(provider,
-        "* { background-color: #222222; color: #ffffff; }",
-        -1, NULL);
-
+        "* { background-color: #222222; color: #ffffff; }", -1, NULL);
     GtkStyleContext *context = gtk_widget_get_style_context(window);
     gtk_style_context_add_provider(context,
         GTK_STYLE_PROVIDER(provider),
         GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-    // Content: just a label for now
+    // Content
     GtkWidget *label = gtk_label_new("GTK Panel");
     gtk_container_add(GTK_CONTAINER(window), label);
 
-    // Exit cleanly
+    // Clean exit
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_widget_show_all(window);
